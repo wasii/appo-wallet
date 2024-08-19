8d6309b3c76ce029164f57569bd2b3092ffc8eaf
AbdWasiq <khatri.wasiq@gmail.com>
2024-08-19 13:30:02 +0500

    Registration view viewmodel initialised

---
+++ AppO/Core/Startup/Registration/View/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  AppO
+//
+//  Created by Abul Jaleel on 19/08/2024.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
---
+++ AppO/Core/Startup/Registration/ViewModel/RegistrationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationViewModel.swift
+//  AppO
+//
+//  Created by Abul Jaleel on 19/08/2024.
+//
+
+import Foundation
