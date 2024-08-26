51a43bf968a1b77ad54513ab4a3d8d9396fc18c6
AbdWasiq <khatri.wasiq@gmail.com>
2024-08-26 23:49:15 +0500

    replace card view initiated

---
+++ AppO/Module/Dashboard/SideMenu/Settings/Replace Card/ReplaceCardView.swift
@@ -0,0 +1,37 @@
+//
+//  ReplaceCardView.swift
+//  AppO
+//
+//  Created by Abul Jaleel on 26/08/2024.
+//
+
+import SwiftUI
+
+struct ReplaceCardView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            NavigationBarView(title: "Replace Card")
+            
+            CardButtonView(cardType: "Visa Gold Card", cardNumber: "423671******0129" ,cardExpiry: "01/30")
+            
+            Spacer()
+            
+            Button {} label: {
+                Text("Submit")
+                    .font(.title3)
+                    .fontWeight(.medium)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 60)
+                    .background(Color(.appBlue))
+                    .clipShape(Capsule())
+                    .foregroundStyle(Color.white)
+            }
+        }
+        .padding()
+        .background(Color.appBackground)
+    }
+}
+
+#Preview {
+    ReplaceCardView()
+}
---
+++ AppO/Module/Dashboard/SideMenu/Settings/Replace Card/ReplaceCardViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ReplaceCardViewModel.swift
+//  AppO
+//
+//  Created by Abul Jaleel on 26/08/2024.
+//
+
+import Foundation
+
+class ReplaceCardViewModel: ObservableObject {
+    
+}
