820df0f32680cd4aabd79d9bd65b0e94c3f3dbf0
AbdWasiq <khatri.wasiq@gmail.com>
2024-09-10 12:35:50 +0500

    transaction details view revamped

--- AppO/Module/Dashboard/SideMenu/Manage Account/Card Transactions/CardTransactionDetailsView.swift
+++ AppO/Module/Dashboard/SideMenu/Manage Account/Card Transactions/CardTransactionDetailsView.swift
@@ -15,36 +15,38 @@ struct CardTransactionDetailsView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             NavigationBarView(title: "Transaction Details")
-            
-            Text("Mini Statement")
-                .foregroundStyle(Color.appBlue)
-                .fontWeight(.semibold)
-            HStack {
-                Text("Available Balance:")
-                Text("$123.45")
-            }
-            .foregroundColor(Color.black.opacity(0.7))
-            ScrollView {
-                ForEach(transactionDetailsItems, id: \.self) { item in
-                    VStack(alignment:.leading, spacing: 10) {
-                        HStack {
-                            Text(item.dateTime)
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Mini Statement")
+                    .foregroundStyle(Color.appBlue)
+                    .fontWeight(.semibold)
+                HStack {
+                    Text("Available Balance:")
+                    Text("$123.45")
+                }
+                .foregroundColor(Color.black.opacity(0.7))
+                ScrollView {
+                    ForEach(transactionDetailsItems, id: \.self) { item in
+                        VStack(alignment:.leading, spacing: 10) {
+                            HStack {
+                                Text(item.dateTime)
+                                    .font(.footnote)
+                                Spacer()
+                                Text(item.price)
+                                    .foregroundStyle(Color.appOrange)
+                            }
+                            Text(item.details)
                                 .font(.footnote)
-                            Spacer()
-                            Text(item.price)
-                                .foregroundStyle(Color.appOrange)
                         }
-                        Text(item.details)
-                            .font(.footnote)
-                    }
-                    .padding()
-                    .overlay(RoundedRectangle(cornerRadius: 5)
+                        .padding()
+                        .overlay(RoundedRectangle(cornerRadius: 5)
                             .stroke(Color.appBlue, lineWidth: 1))
+                    }
                 }
             }
+            .padding()
             Spacer()
         }
-        .padding()
+        .edgesIgnoringSafeArea(.top)
         .background(Color.appBackground)
         .toolbar(.hidden, for: .navigationBar)
     }
