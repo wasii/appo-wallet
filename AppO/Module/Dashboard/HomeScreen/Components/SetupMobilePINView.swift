bc841b9433718f5c1aab7309993c797fd5325849
AbdWasiq <khatri.wasiq@gmail.com>
2024-09-25 11:54:04 +0500

    setup card pin view initiated

---
+++ AppO/Module/Dashboard/HomeScreen/Components/SetupMobilePINView.swift
@@ -0,0 +1,18 @@
+//
+//  SetupMobilePINView.swift
+//  AppO
+//
+//  Created by Abul Jaleel on 25/09/2024.
+//
+
+import SwiftUI
+
+struct SetupMobilePINView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetupMobilePINView()
+}
